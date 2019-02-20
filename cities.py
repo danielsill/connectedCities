0f23fb1e77b41c0fb6715a2dd02757f25edd2ff5
Daniel Sill <sill.danield@gmail.com>
2019-02-20 11:54:45 -0500

    added factoring function

--- cities.py
+++ cities.py
@@ -1,1 +1,17 @@
+import math
 
+
+def getThresholdFactors(n, threshold):
+    small_factors = []
+    large_factors = []
+    for i in range(2, math.floor(math.sqrt(n)) + 1):
+        if n % i == 0:
+            if i >= threshold:
+                small_factors.append(i)
+            factor_pair = n//i
+            if factor_pair != i:
+                large_factors.append(factor_pair)
+    return small_factors + large_factors[::-1]
+
+
+print(getThresholdFactors(64, 2))
